136899811441b6eb582f4038949dba5a12dc180c
Patrick McLaughlin <psm14case@gmail.com>
2012-04-18 00:44:54 -0400

    Refactor some things, remove dead code

--- blackjack.hs
+++ blackjack.hs
@@ -30,26 +30,6 @@ data Card = Card Rank Suit
      deriving (Show, Read, Eq, Ord)
 
 type Deck = [Card]
-type Hand = [Card]
-
-data DealerHand = DealerHand Card Hand
-     deriving (Show, Read, Eq)
-
-data HandStatus = Hard
-                | Soft
-                | Bust
-     deriving (Show, Read, Eq)
-
-data HandValue = HandValue HandStatus Int
-     deriving (Show, Read, Eq)
-
--- Note that this only is well-defined for hand values that are valid in BlackJack (i.e Using a Soft 2 or a Bust 10 will have strange behavior)
-instance Monoid HandValue where
-  mempty  = HandValue Hard 0
-  mappend (HandValue Soft m) (HandValue t n)    = if (m+n <= 21) then HandValue Soft (m+n) else mappend (HandValue Hard (m-10)) (HandValue t n) 
-  mappend (HandValue t m) (HandValue Soft n)    = mappend (HandValue Soft n) (HandValue t m)
-  mappend (HandValue Hard m) (HandValue Hard n) = if (m+n <= 21) then HandValue Hard (m+n) else HandValue Bust (m+n)
-  mappend (HandValue t m) (HandValue u n)       = HandValue Bust (m+n)
 
 stdDeck :: Deck
 stdDeck = [Card r s | r <- [Ace .. King], s <- [Clubs .. Diamonds]]
@@ -73,6 +53,29 @@ randInfShuffle :: Deck -> IO Deck
 randInfShuffle deck = newStdGen >>= doShuffle deck
   where doShuffle deck gen = return $ infiniteDeck deck gen
 
+data HandStatus = Play
+                | Done
+     deriving (Show)
+
+data Hand = Hand HandStatus [Card]
+     deriving (Show)
+
+data HandType = Hard
+              | Soft
+              | Bust
+     deriving (Show, Read, Eq)
+
+data HandValue = HandValue HandType Int
+     deriving (Show, Read, Eq)
+
+-- Note that this only is well-defined for hand values that are valid in BlackJack (i.e Using a Soft 2 or a Bust 10 will have strange behavior)
+instance Monoid HandValue where
+  mempty  = HandValue Hard 0
+  mappend (HandValue Soft m) (HandValue t n)    = if (m+n <= 21) then HandValue Soft (m+n) else mappend (HandValue Hard (m-10)) (HandValue t n) 
+  mappend (HandValue t m) (HandValue Soft n)    = mappend (HandValue Soft n) (HandValue t m)
+  mappend (HandValue Hard m) (HandValue Hard n) = if (m+n <= 21) then HandValue Hard (m+n) else HandValue Bust (m+n)
+  mappend (HandValue t m) (HandValue u n)       = HandValue Bust (m+n)
+
 cardTotal :: Card -> HandValue
 cardTotal n = case n of
   Card Two _   -> HandValue Hard 2
@@ -89,25 +92,18 @@ cardTotal n = case n of
   Card King _  -> HandValue Hard 10
   Card Ace _   -> HandValue Soft 11
 
+
 handTotal :: [Card] -> HandValue
 handTotal = mconcat . (map cardTotal)
 
---type ShoeT m a = StateT Deck m a
+handTotal' :: Hand -> HandValue
+handTotal' (Hand _ cs) = handTotal cs
 
---runShoeT :: ShoeT m a -> Deck -> m (a, Deck)
---runShoeT = runStateT
+hand' :: [Card] -> Hand
+hand' cs = Hand (checkDone $ handTotal cs) cs
+  where checkDone (HandValue Bust _) = Done
+        checkDone _                  = Play
 
---deal :: (Monad m) => ShoeT m Card
---deal = do
---  d <- get
---  put $ tail d
---  return $ head d
-
---foobar :: ShoeT Identity ()
---foobar = return ()
-
---foobat :: ShoeT IO ()
---foobat = return ()
 
 type HiLoCount = Int
 
@@ -132,11 +128,7 @@ hiLoTotal = reduce' . map'
   where map'    = map hiLoValue
         reduce' = sum
 
---main = do
---  n <- liftM (hiLoTotal . take 1000000) $ randInfShuffle (multiDeck 6)
---  putStrLn $ show n
-
-data Game = Game DealerHand [Hand]
+data Game = Game Hand [Hand]
   deriving (Show)
 
 deal' :: Int -> State Deck [Card]
@@ -148,16 +140,16 @@ deal = liftM head $ deal' 1
 newGame :: State Deck Game
 newGame = do
   [d_holeCard, p_firstCard, d_upCard, p_secondCard] <- deal' 4
-  dealer <- return $ DealerHand d_holeCard [d_upCard]
-  player <- return $ [p_firstCard, p_secondCard]
+  dealer <- return $ hand' [d_holeCard,d_upCard]
+  player <- return $ hand' [p_firstCard, p_secondCard]
   return $ Game dealer [player]
 
 newGame' :: Int -> State Deck Game
 newGame' n = do
   roundOne <- deal' n
   roundTwo <- deal' n
-  dealer   <- return $ DealerHand (head roundOne) [(head roundTwo)]
-  players  <- return $ zipWith (\a b -> [a,b]) (tail roundOne) (tail roundTwo)
+  dealer   <- return $ hand' [(head roundOne), (head roundTwo)]
+  players  <- return $ zipWith (\a b -> hand' [a,b]) (tail roundOne) (tail roundTwo)
   return $ Game dealer players
 
 data Action = Hit
@@ -168,7 +160,7 @@ data Action = Hit
 
 -- Hit Soft 17
 dealerStrategy :: Hand -> Action
-dealerStrategy h = case (handTotal h) of
+dealerStrategy h = case (handTotal' h) of
   HandValue Bust _  -> Stand
   HandValue Soft 18 -> Stand
   HandValue Soft 19 -> Stand
@@ -183,16 +175,18 @@ dealerStrategy h = case (handTotal h) of
   HandValue Hard _  -> Hit
 
 doHit :: Hand -> State Deck [Hand]
-doHit h = deal >>= (\c -> return $ [h ++ [c]])
+doHit (Hand Play hs) = deal >>= (\c -> return $ [hand' (hs ++ [c])])
+doHit _ = undefined
 
 doStand :: Hand -> State Deck [Hand]
-doStand h = return $ [h]
+doStand (Hand Play h) = return $ [Hand Done h]
+doStand _ = undefined
 
 --doDouble :: Hand -> State Deck [Hand]
 --doDouble h = 
 
-play :: Action -> Hand -> State Deck [Hand]
-play a = case a of
-  Hit -> doHit
+--play :: Action -> Hand -> State Deck [Hand]
+--play a = case a of
+--  Hit -> doHit
 
 --play :: Game -> State Deck Game
