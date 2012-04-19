c5d9bb4848fb3bc6f54d3a7caa2a2e684a396f36
Patrick McLaughlin <psm14case@gmail.com>
2012-04-19 02:19:36 -0400

    Make deck an actual type, which opens a possible way to carry a customized count around with it

--- cards.hs
+++ cards.hs
@@ -24,28 +24,37 @@ data Rank = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten |
 data Card = Card Rank Suit
      deriving (Show, Read, Eq, Ord)
 
-type Deck = [Card]
+data Deck = Deck [Card]
+
+instance Show Deck where
+  show d = "Deck"
 
 stdDeck :: Deck
-stdDeck = [Card r s | r <- [Ace .. King], s <- [Clubs .. Diamonds]]
+stdDeck = Deck [Card r s | r <- [Ace .. King], s <- [Clubs .. Diamonds]]
 
 multiDeck :: Int -> Deck -> Deck
-multiDeck n = concat . replicate n
+multiDeck n (Deck d) = Deck . concat . replicate n $ d
 
 shuffle :: RandomGen g => Deck -> g -> Deck
-shuffle d g = shuffle' d (length d) g
+shuffle (Deck d) g = Deck $ shuffle' d (length d) g
 
 shuffleIO :: Deck -> IO Deck
 shuffleIO d = liftM (shuffle d) newStdGen
 
 infiniteDeck :: Deck -> Deck
-infiniteDeck d = concat decks
+infiniteDeck (Deck d) = Deck $ concat decks
    where decks = d : decks
 
 infiniteShuffler :: RandomGen g => Deck -> g -> Deck
-infiniteShuffler d g = concat $ decks g
+infiniteShuffler (Deck d) g = Deck $ concat $ decks g
        where decks g = d : decks g'
              g'      = snd $ split g
 
 infiniteShufflerIO :: Deck -> IO Deck
 infiniteShufflerIO d = liftM (infiniteShuffler d) newStdGen
+
+-- TODO? Make Deck into a typeclass so that other implementations can include a count
+deal :: Deck -> (Card, Deck)
+deal (Deck d) = (card, rest)
+   where card = head d
+         rest = Deck $ tail d
