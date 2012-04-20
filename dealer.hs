7569579e6349e565857e52a3b036edeaeef2947e
Patrick McLaughlin <psm14case@gmail.com>
2012-04-19 22:54:14 -0400

    Add dealer strategies

--- dealer.hs
+++ dealer.hs
@@ -9,10 +9,30 @@ import Control.Monad.State
 
 dealDealer :: Deck d => d -> (DealerHand, d)
 dealDealer d = let
-	(down, d') = dealDown d
-	(up,  d'') = deal d'
-	hand       = DealerHand down up
-	in (hand, d'')
+  (down, d') = dealDown d
+  (up,  d'') = deal d'
+  hand       = DealerHand down up
+  in (hand, d'')
 
 dealDealerS :: Deck d => State d DealerHand
 dealDealerS = liftS dealDealer
+
+-- Dealer only hits or stands
+data DealerAction = Hit | Stand
+
+-- Dealer strategies
+type DealerStrategy = Hand -> DealerAction
+
+hitS17 :: DealerStrategy
+hitS17 h = act (handTotal h)
+  where act (HandValue Bust _)           = Stand
+        act (HandValue Soft n) | n <  18 = Hit
+                               | n >= 18 = Stand
+        act (HandValue Hard n) | n <  17 = Hit
+                               | n >= 17 = Stand
+
+standS17 :: DealerStrategy
+standS17 h = act (handTotal h)
+  where act (HandValue Bust _)           = Stand
+        act (HandValue _    n) | n <  18 = Hit
+                               | n >= 18 = Stand
